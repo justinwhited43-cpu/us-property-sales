0f9107051802856596e183351870689107aa7a5e
justinwhited43-cpu <justinwhited43@gmail.com>
2025-12-15 01:35:35 -0500

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>US Property Sales</h1>
-      <p>Starting with Clark County, Ohio</p>
-    </main>
-  );
-}
